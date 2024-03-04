f31f3ea4bd504fd075f1ec7ecbe8ec3dee9f09b6
Ryan Jones-Ward <sciguyryan@gmail.com>
2024-03-04 14:41:04 +0000

    Glowstone now matches GregTech too.
    Note that we are maintaining the 8 Chemlib items = 500ml of fluid (where applicable). GregTech handles 10 redstone as producing 3 buckets of mercury. At 16 per bucket, that gives the 48 per item seen here.

--- kubejs/server_scripts/mods/alchemistry/remove.js
+++ kubejs/server_scripts/mods/alchemistry/remove.js
@@ -4,6 +4,10 @@ ServerEvents.recipes((event) => {
     { id: "alchemistry:dissolver/redstone_block" },
     { id: "alchemistry:combiner/redstone" },
     { id: "alchemistry:combiner/redstone_block" },
+    { id: "alchemistry:dissolver/glowstone_dust" },
+    { id: "alchemistry:dissolver/glowstone" },
+    { id: "alchemistry:combiner/glowstone_dust" },
+    { id: "alchemistry:combiner/glowstone" },
   ];
 
   removals.forEach((removal) => {
