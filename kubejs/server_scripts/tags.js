46ea8f6284f7ef6a165f190e32888ca91deae4a8
Ryan Jones-Ward <sciguyryan@gmail.com>
2024-03-04 16:51:48 +0000

    Add Almost Unified to help unify certain ingredients.

---
+++ kubejs/server_scripts/tags.js
@@ -0,0 +1,11 @@
+ServerEvents.tags("block", (event) => {
+});
+
+ServerEvents.tags("entity_type", (event) => {
+});
+
+ServerEvents.tags("fluid", (event) => {
+});
+
+ServerEvents.tags("item", (event) => {
+});
