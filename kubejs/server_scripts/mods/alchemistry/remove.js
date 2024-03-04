fce4b126eff58dbbf8165073037c68e8a9e706db
Ryan Jones-Ward <sciguyryan@gmail.com>
2024-03-04 19:30:25 +0000

    More work and more consistency fixes.

--- kubejs/server_scripts/mods/alchemistry/remove.js
+++ kubejs/server_scripts/mods/alchemistry/remove.js
@@ -1,13 +1,18 @@
 ServerEvents.recipes((event) => {
   const removals = [
-    { id: "alchemistry:dissolver/redstone" },
-    { id: "alchemistry:dissolver/redstone_block" },
     { id: "alchemistry:combiner/redstone" },
+    { id: "alchemistry:dissolver/redstone" },
     { id: "alchemistry:combiner/redstone_block" },
+    { id: "alchemistry:dissolver/redstone_block" },
     { id: "alchemistry:dissolver/glowstone_dust" },
-    { id: "alchemistry:dissolver/glowstone" },
     { id: "alchemistry:combiner/glowstone_dust" },
     { id: "alchemistry:combiner/glowstone" },
+    { id: "alchemistry:dissolver/glowstone" },
+
+    // TODO - ensure that all of the Alchemistry dusts behave this way.
+    { id: "alchemistry:combiner/iron_disulfide_dust" },
+    { id: "alchemistry:compactor/iron_disulfide_dust" },
+    { id: "alchemistry:dissolver/iron_disulfide_dust" },
   ];
 
   removals.forEach((removal) => {
--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -8,4 +8,5 @@ ServerEvents.tags("fluid", (event) => {
 });
 
 ServerEvents.tags("item", (event) => {
+    event.add('forge:dusts/pyrite', 'chemlib:iron_disulfide_dust');
 });
