104bcc9d9e3f68de24707ef4a796cd4cccb3aff5
Ryan Jones-Ward <sciguyryan@gmail.com>
2024-03-08 15:11:10 +0000

    Another batch of recipes.

--- kubejs/server_scripts/mods/gtceu/electrolyzer.js
+++ kubejs/server_scripts/mods/gtceu/electrolyzer.js
@@ -1,11 +1,16 @@
+// NOTE - around 9 seconds per input dust used as a basis.
+const INPUT_MULTIPLIER = 9;
+
+const EU_PER_TICK_LV = 30;
+
 ServerEvents.recipes((event) => {
   event.recipes.gtceu
     .electrolyzer('dusts/ilmenite')
     .itemInputs('5x #forge:dusts/ilmenite')
     .itemOutputs('1x gtceu:iron_dust', '1x gtceu:titanium_dust')
     .outputFluids(Fluid.of('gtceu:liquid_oxygen', 3000))
-    .duration(100)
-    .EUt(60);
+    .duration(INPUT_MULTIPLIER * 5)
+    .EUt(EU_PER_TICK_LV);
 });
 
 ServerEvents.recipes((event) => {
@@ -14,6 +19,16 @@ ServerEvents.recipes((event) => {
     .itemInputs('3x #forge:dusts/rutile')
     .itemOutputs('1x gtceu:titanium_dust')
     .outputFluids(Fluid.of('gtceu:liquid_oxygen', 2000))
-    .duration(100)
-    .EUt(60);
+    .duration(INPUT_MULTIPLIER * 3)
+    .EUt(EU_PER_TICK_LV);
+});
+
+ServerEvents.recipes((event) => {
+  event.recipes.gtceu
+    .electrolyzer('dusts/scheelite')
+    .itemInputs('5x #forge:dusts/scheelite')
+    .itemOutputs('1x gtceu:calcium_dust', '1x gtceu:tungsten_dust')
+    .outputFluids(Fluid.of('gtceu:liquid_oxygen', 4000))
+    .duration(INPUT_MULTIPLIER * 5)
+    .EUt(EU_PER_TICK_LV);
 });
--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,15 +1,19 @@
-ServerEvents.tags("block", (event) => {});
+ServerEvents.tags('block', (event) => {});
 
-ServerEvents.tags("entity_type", (event) => {});
+ServerEvents.tags('entity_type', (event) => {});
 
-ServerEvents.tags("fluid", (event) => {});
+ServerEvents.tags('fluid', (event) => {});
 
-ServerEvents.tags("item", (event) => {
-  event.add("forge:dusts/pyrite", "chemlib:iron_disulfide_dust");
-  event.add("forge:dusts/cassiterite", "chemlib:tin_oxide_dust");
-  event.add("forge:dusts/calcite", "chemlib:calcium_carbonate_dust");
-  event.add("forge:dusts/galena", "chemlib:lead_sulfide_dust");
-  event.add("forge:dusts/garnierite", "chemlib:nickel_oxide_dust");
-  event.add("forge:dusts/green_sapphire", "chemlib:aluminum_oxide_dust");
-  event.add("forge:dusts/magnesite", "chemlib:magnesium_carbonate_dust");
+ServerEvents.tags('item', (event) => {
+  event.add('forge:dusts/pyrite', 'chemlib:iron_disulfide_dust');
+  event.add('forge:dusts/cassiterite', 'chemlib:tin_oxide_dust');
+  event.add('forge:dusts/calcite', 'chemlib:calcium_carbonate_dust');
+  event.add('forge:dusts/galena', 'chemlib:lead_sulfide_dust');
+  event.add('forge:dusts/garnierite', 'chemlib:nickel_oxide_dust');
+  event.add('forge:dusts/green_sapphire', 'chemlib:aluminum_oxide_dust');
+  event.add('forge:dusts/magnesite', 'chemlib:magnesium_carbonate_dust');
+  event.add('forge:dusts/pyrolusite', 'chemlib:manganese_oxide_dust');
+  event.add('forge:dusts/saltpeter', 'chemlib:potassium_nitrate_dust');
+  event.add('forge:dusts/sphalerite', 'chemlib:zinc_sulfide_dust');
+  event.add('forge:dusts/stibnite', 'chemlib:antimony_trisulfide_dust');
 });
